cc9d749bc39108771ce4fe91baeab135eb6ef738
donamKim <donam.kim@nerdspace.co.kr>
2021-02-25 23:00:27 +0900

    -

--- text/__init__.py
+++ text/__init__.py
@@ -9,7 +9,7 @@ from hparams import hparams
 from text.symbols import symbols, en_symbols, PAD, EOS
 from text.korean import jamo_to_korean
 
-nltk.download()
+nltk.download('punkt')
 
 # Mappings from symbol to numeric ID and vice versa:
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}   # 80개
